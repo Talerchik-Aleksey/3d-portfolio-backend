1d8225b5ee92ab9e773938260201ceb17ffdd315
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-06-06 23:34:41 +0300

    up limited

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ const app = express();
 async function startServer() {
   try {
     await connect();
-    app.use(express.json({ limit: "50mb" }));
-    app.use(express.urlencoded({ limit: "50mb" }));
+    app.use(express.json({ limit: "100mb" }));
+    app.use(express.urlencoded({ limit: "100mb" }));
     app.use(cors());
     app.use(
       pinoHttp({
