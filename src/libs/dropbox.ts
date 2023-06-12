6cf7911b150c1d1e5c1d4dbdcfae3f65e05660aa
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-06-12 09:33:25 +0300

    Revert "fix: dropbox connect with oAuth"
    This reverts commit 1e5ed184cacd0d28c587cfd591e038f7d5c28655.

--- src/libs/dropbox.ts
+++ src/libs/dropbox.ts
@@ -4,15 +4,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const appKey = process.env.DROPBOX_APP_KEY;
-const appSecret = process.env.DROPBOX_APP_SECRET;
-
 const dbx = new Dropbox({
-  clientId: appKey,
-  clientSecret: appSecret,
+  customHeaders: {
+    Authorization: `Bearer ${process.env.DROPBOX_ACCESS_TOKEN}`,
+  },
   fetch,
 });
 
+// Now you can use dbx to interact with Dropbox...
+//
 export async function uploadFile(file: any, name: string) {
   const res = await dbx.filesUpload({
     path: `/3d-protfolio/${name}.glb`,
